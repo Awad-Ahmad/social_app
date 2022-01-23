5a3c834f7af27ee8c511cf789307b2ddc8f00ae0
Ahmad Awad <ahmadmohanasawad@gmail.com>
2022-01-23 13:44:31 +0200

    20002

--- lib/modules/Rigester/RigeserCubit.dart
+++ lib/modules/Rigester/RigeserCubit.dart
@@ -40,7 +40,7 @@ class SocialRegisterCubit extends Cubit< SocialRegisterState> {
   IconData suffix = Icons.visibility_outlined;
   bool isPasswordShown = true;
 //
-  //hello from ahmad 0934961515
+  //hello from ahmad 2002
   void changePasswordVisibilty() {
     isPasswordShown = !isPasswordShown;
     suffix = isPasswordShown ? Icons.visibility_outlined : Icons.visibility_off;
